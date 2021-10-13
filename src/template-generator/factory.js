3a4ca7daf65b298db0091a03b9979b338943f039
Bas van Rossem <bas.v.rossem@gmail.com>
2021-10-13 22:40:32 +0200

    Added row class
    Feature: Align rows beneath eachother
    Feature: Align description boxes to the left of the elements

--- src/template-generator/factory.js
+++ src/template-generator/factory.js
@@ -1,3 +1,106 @@
+class Row {
+    name;
+    descriptionText;
+    description;
+    elements;
+    link;
+    participants;
+
+    // Row position data
+    left = Infinity;
+    down = -Infinity;
+    columnPadding = 50;
+    rowPadding = 50;
+    
+    allElements() {
+        let all = [];
+        if(this.description) all.push(this.description);
+        if(this.elements && this.elements.length) all.push(this.elements);
+        all = all.flat(Infinity);
+        return all;
+    }
+
+    constructor(data, participants = 1) {
+        this.name = data.name;
+        this.link = data.link;
+        this.participants = participants;
+        this.elements = this.build(data.elements);
+        
+        this.description = this.buildDesciption(data.description);
+    }
+
+    build(elementData) {
+        if(!elementData || !elementData.length) return [];
+        
+        const elements = [];
+        
+        elementData.forEach(ed => {
+            const amount = ed.repeating ? this.participants: 1; 
+            for(let i = 0; i < amount; i++) { 
+                elements.push({...ed}); 
+            }
+        })
+
+        return elements;
+    }
+
+    buildDesciption(descriptionText) {
+        let descriptionElement = undefined;
+        if(descriptionText) {
+            const description = {
+                type: "shape",
+                text: descriptionText,
+                width: 300,
+                x: 0,
+                y: 0,
+                style: {
+                    backgroundColor: "#ADDBD2",
+                    backgroundOpacity: 0.3,
+                    shapeType: miro.enums.shapeType.ROUNDER
+                },
+            };
+            descriptionElement = description;
+        }
+        return descriptionElement;
+    }
+
+    positionElementsX(){
+        if(!this.elements || !this.elements.length) return;
+
+        // Position elements besides eachother
+        let nextRight = 0;
+        this.elements.forEach(e => {
+            e.x = nextRight;
+            nextRight += e.width + this.columnPadding; 
+        });
+
+        // Position the elements to the middle of the page
+        this.elements = this.elements.flat(Infinity);
+        const elementsWidth = this.elements[this.elements.length - 1].x - this.elements[0].x;
+        const elementsOffset = elementsWidth / 2;
+        this.elements.forEach(e => e.x -= elementsOffset);
+
+        // Find left most x pos
+        this.elements.forEach(e => this.left = e.x < this.left ? e.x : this.left);
+    };
+
+    positionElementsY(nextY){
+        if(this.description) this.description.y = nextY;
+        if(this.elements && this.elements.length != 0) this.elements.forEach(e => e.y = nextY);
+
+        let maxHeight = 100; // We assume that 100 is the default element height
+        this.allElements().forEach(e => maxHeight = e.height > maxHeight ? e.height : maxHeight);
+
+        return nextY + maxHeight + this.rowPadding; 
+    };
+
+    positionDescriptionElement(newX){
+        if(this.description) {
+            this.description.x = newX - this.description.width;
+        }
+    };
+}
+
 class Factory {
     rowPosY = 0;
     rowPadding = 50;
